aad632db44ce3bc878c672e81673b04ace82a936
Nosakhare Osaro <osaronosakhare@gmail.com>
2026-01-19 14:35:23 +0000

    Exon Model Created

--- src/gene_summariser/models.py
+++ src/gene_summariser/models.py
@@ -15,3 +15,17 @@ class Feature:
     def length(self) -> int:
         """Return the length of the feature."""
         return self.end - self.start + 1
+    
+@dataclass
+class Exon(Feature):
+    """Represents an exon."""
+    
+    exon_id: str = ""
+    
+    def overlaps(self, other: "Exon") -> bool:
+        """Check if this exon overlaps with another exon on the same strand."""
+        if self.seqid != other.seqid or self.strand != other.strand:
+            return False
+        return not (self.end < other.start or self.start > other.end)
+
+
