791f793d9af5df0c9f8a791c89f33c4f99915ca9
Callum Poole <CallumPoole@protonmail.com>
2026-01-20 02:17:27 +0000

    Adding docstring and comments where required. Docstring format allows help() to be used to gain better understanding of class and methods without opening the module

--- src/gene_summariser/praser.py
+++ src/gene_summariser/praser.py
@@ -6,8 +6,29 @@ from gene_summariser.models import Transcript, Exon, CDS, Feature
 
 
 class ParserGFF:
+    """
+    PraserGFF provides the nessesary methods to parse a specified GFF file, extracting Gene, transcript, exon, and CDS information.
+
+    Attributes:
+        gff_path (str): Path to the GFF file to be parsed.
+        db (gffutils.FeatureDB): GFF database object for efficient querying of features.
+
+    Methods:
+        parse_transcripts() -> list[Transcript]: Parses the GFF file and returns a list of Transcript objects, each containing associated exons and CDS features.
+        parse_genes() -> list[Gene]: Parses the GFF file and returns a list of Gene features. (To be implemented)
+    """
+
     def __init__(self, gff_path: str) -> None:
+        """
+        Creating the GFF database if it does not exist, otherwise loading the existing database.
+
+        Args:
+            gff_path (str): Path to the GFF file to be parsed.
+        Raises:
+            FileNotFoundError: If the specified GFF file does not exist.
+        """
         self.gff_path = gff_path
+        # Creating the database path from the GFF file path, this is used to both find or create the database.
         db_path = os.path.splitext(self.gff_path)[0] + ".db"
 
         if os.path.exists(db_path):
@@ -27,13 +48,21 @@ class ParserGFF:
                 )
 
     def parse_transcripts(self) -> list[Transcript]:
-
+        """
+        Parses the GFF file and returns a list of Transcript objects, format of which was defined in models.py.
+        Returns:
+            list[Transcript]: A list of Transcript objects with associated exons and CDS features.
+        Raises:
+            (To be implemented)
+        """
         transcripts: list[Transcript] = []
+        # In GFF3 files, mRNA features represent transcripts.
         for feature in self.db.features_of_type("mRNA"):
 
             exons = []
             cds_features = []
 
+            # Creating the list of Exon and CDS objects associated to the current transcript.
             for exon in self.db.children(feature, featuretype="exon"):
                 exons.append(
                     Exon(
@@ -69,6 +98,7 @@ class ParserGFF:
                 cds_features=cds_features,
                 attributes=dict(feature.attributes),
             )
+
             transcripts.append(transcript)
 
         return transcripts
