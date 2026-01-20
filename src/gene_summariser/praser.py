8fab6cb3621d1387b57914dd71fa0f73d2df1702
Callum Poole <CallumPoole@protonmail.com>
2026-01-20 00:59:27 +0000

    Updating file path to use path provided by the user (works better if project is run in different dir). Additionaly ensured the db is created using the gff_file rather than using memory

--- src/gene_summariser/praser.py
+++ src/gene_summariser/praser.py
@@ -5,17 +5,16 @@ import gffutils
 
 class ParserGFF:
     def __init__(self, gff_file: str) -> None:
-        self.gff_path = os.path.join(os.getcwd(), gff_file)
+        self.gff_path = gff_file
+        db_path = os.path.splitext(self.gff_path)[0] + ".db"
 
-        if os.path.exists(os.path.splitext(self.gff_path)[0] + ".db"):
-            self.db = gffutils.FeatureDB(
-                os.path.splitext(self.gff_path)[0] + ".db", keep_order=True
-            )
+        if os.path.exists(db_path):
+            self.db = gffutils.FeatureDB(db_path, keep_order=True)
         else:
             try:
                 self.db = gffutils.create_db(
                     self.gff_path,
-                    dbfn=":memory:",
+                    dbfn=db_path,
                     force=True,
                     keep_order=True,
                     sort_attribute_values=True,
