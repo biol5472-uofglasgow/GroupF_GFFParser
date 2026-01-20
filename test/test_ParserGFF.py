7e962c37854577a39da81c8a101a83cbd0a21e10
Callum Poole <CallumPoole@protonmail.com>
2026-01-20 17:20:53 +0000

    adding basic test to set up pytest correctly, checking that no transcripts exist with a len of 0

--- test/test_ParserGFF.py
+++ test/test_ParserGFF.py
@@ -6,3 +6,7 @@ from gene_summariser.praser import ParserGFF
 def parser():
     gff_path = "fixture_data/models.gff3"
     return ParserGFF(gff_path)
+
+def test_parse_transcript(parser):
+    transcripts = parser.parse_transcripts
+    assert len(transcripts) > 0 
