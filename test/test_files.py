e403068c505cb106b903ddff9f2884fbf48872b1
ABEL BIJU DANIEL <132029020+DarkATOM7516@users.noreply.github.com.>
2026-01-21 12:34:42 +0000

    added test for file type

---
+++ test/test_files.py
@@ -0,0 +1,9 @@
+# def validate_inputs(args):
+#     if not os.path.isfile(args.gff):
+#         sys.exit(f"GFF file not found: {args.gff}")
+
+#     if args.fasta and not os.path.isfile(args.fasta):
+#         sys.exit(f"FASTA file not found: {args.fasta}")
+
+#     if not args.gff.endswith((".gff", ".gff3")):
+#         sys.exit("Input file is not a GFF/GFF3 file")
