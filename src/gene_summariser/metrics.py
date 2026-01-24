15f4906c10dc15fb57f834198d4368078f52c914
Nosakhare Osaro <osaronosakhare@gmail.com>
2026-01-24 19:17:27 +0000

    create metrics calculator class

--- src/gene_summariser/metrics.py
+++ src/gene_summariser/metrics.py
@@ -1,2 +1,13 @@
 from gene_summariser.models import Transcript, TranscriptSummary
 from gene_summariser.qc import QCChecker
+
+class MetricsCalculator:
+    """Calculate summary metrics and QC flags for transcripts.
+    
+    This class coordinates between QC checking and summary creation,
+    producing TranscriptSummary objects that include both metrics
+    and quality control flags.
+    
+    Attributes:
+        qc_checker: QCChecker instance for running quality checks
+    """
