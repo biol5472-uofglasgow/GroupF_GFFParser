780929b47bb31cc13b384ad071ef300a2cd33241
Callum Poole <CallumPoole@protonmail.com>
2026-01-24 13:27:20 +0000

    fixing naming errors across qc, summariser and unit tests

--- src/gene_summariser/qc.py
+++ src/gene_summariser/qc.py
@@ -1,6 +1,6 @@
-from gene_summariser.praser import ParserGFF
-from gene_summariser.summariser import make_summary
 from gene_summariser.models import Transcript, TranscriptSummary
+from gene_summariser.Summariser import make_summary
+
 
 def flag_devisable_by_three(transcript: Transcript, summary: TranscriptSummary) -> None:
     """
@@ -19,33 +19,35 @@ def flag_devisable_by_three(transcript: Transcript, summary: TranscriptSummary) 
         summary.flags.append("CDS_NOT_DEVISABLE_BY_3")
 
 
-
-#QC for phase incosistency
-'''
+# QC for phase incosistency
+"""
 This fucntion performs a quality check to ensure that all cds within a transcript maintain a 
 consistent reading frame
-'''
-def check_cds_phase_errors(transcript:Transcript, summary: TranscriptSummary ) -> None: 
-    
-    cds_list=transcript.cds_features
-    
-    #if a transcript has fewer than 2 cds then it will skip it
-    if len(cds_list)<2:
-        return 
-    #and for function with multiple cds it iterates over each consecutive pair of cds
-    for i in range (len(cds_list)-1):
-        current_cds= cds_list[i]
-        next_cds=cds_list[i+1]
+"""
 
-        cds_length= current_cds.length
-        expected_next_phase=(current_cds.phase+ cds_length)%3 #calculates the next cds phase 
-       
-        if next_cds.phase != expected_next_phase: #if the next phase not equal to expected phase throw an error
+
+def check_cds_phase_errors(transcript: Transcript, summary: TranscriptSummary) -> None:
+
+    cds_list = transcript.cds_features
+
+    # if a transcript has fewer than 2 cds then it will skip it
+    if len(cds_list) < 2:
+        return
+    # and for function with multiple cds it iterates over each consecutive pair of cds
+    for i in range(len(cds_list) - 1):
+        current_cds = cds_list[i]
+        next_cds = cds_list[i + 1]
+
+        cds_length = current_cds.length
+        expected_next_phase = (
+            current_cds.phase + cds_length
+        ) % 3  # calculates the next cds phase
+
+        if (
+            next_cds.phase != expected_next_phase
+        ):  # if the next phase not equal to expected phase throw an error
             summary.flags.append("CDS_Phase_Incosistent")
             break
-    
-    
-
 
 
 def append_flags_to_summary(transcripts: list[Transcript]) -> list[TranscriptSummary]:
@@ -62,4 +64,3 @@ def append_flags_to_summary(transcripts: list[Transcript]) -> list[TranscriptSum
         flag_devisable_by_three(transcript, summary)
         summaries.append(summary)
     return summaries
-
