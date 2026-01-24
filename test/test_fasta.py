1a57301587292d9862edabe067c4ec423cfba5c7
Callum Poole <CallumPoole@protonmail.com>
2026-01-24 14:32:32 +0000

    Updating tests to have the correct expected fasta seqeunces

--- test/test_fasta.py
+++ test/test_fasta.py
@@ -16,18 +16,18 @@ def parser(gff_file):
 
 def test_extract_gene_sequences(parser):
     transcripts = parser.parse_transcripts()
-    fasta_file = "test/test_sequences.fasta"
+    fasta_file = "test/testfasta.fasta"
     transcript = transcripts[0]
     cds_parts = list(iter_cds_sequences(fasta_file, transcript))
 
-    assert cds_parts[0] == "GTACGTACGTA"
-    assert cds_parts[1] == "ACGTACGTACGTACGTACG"
-    assert cds_parts[2] == "TACGTACGTACGTAC"
+    assert cds_parts[0] == "CGTACGTACGT"
+    assert cds_parts[1] == "TACGTACGTACGTACGTAC"
+    assert cds_parts[2] == "ACGTACGTACGTACG"
 
 
 def test_phase(parser):
     transcripts = parser.parse_transcripts()
-    fasta_file = "test/test_sequences.fasta"
+    fasta_file = "test/testfasta.fasta"
     transcript = transcripts[1]
     cds_parts = list(iter_cds_sequences(fasta_file, transcript))
 
@@ -36,7 +36,7 @@ def test_phase(parser):
 
 def test_full_transcript(parser):
     transcripts = parser.parse_transcripts()
-    fasta_file = "test/test_sequences.fasta"
+    fasta_file = "test/testfasta.fasta"
     transcript = transcripts[2]
     cds_parts = list(iter_cds_sequences(fasta_file, transcript))
 
