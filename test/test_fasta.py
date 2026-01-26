4dac4544bcfad8d0a184cc17031ecf5fe6ee0645
Callum Poole <CallumPoole@protonmail.com>
2026-01-26 19:50:44 +0000

    Updating test gene sequence extraction to not worry about phase, this was decided due to phase never actualy being use in tandem with the fasta, and extrcting the raw sequence still confirms sucessful fasta parsing

--- test/test_fasta.py
+++ test/test_fasta.py
@@ -1,7 +1,7 @@
 import pytest
 
 from Bio import SeqIO
-from gene_summariser.parser import ParserGFF\
+from gene_summariser.parser import ParserGFF
 from gene_summariser.fasta import get_full_sequence
 
 
@@ -27,25 +27,20 @@ def test_extract_gene_sequences(parser):
 
     assert full_cds == (
         "TTGACGTACGT"
-        "TACGTACGTACGTACGTAC"
-        "ACGTACGTACGTTGC"
+        "CGTACGTACGTACGTACGTAC"
+        "TACGTACGTACGTTGC"
     )
 
-
-def test_phase(parser):
-    transcripts = parser.parse_transcripts()
-    fasta_file = "test/fixtures/testfasta.fasta"
-    transcript = transcripts[0]
-    cds_parts = list(iter_cds_sequences(fasta_file, transcript))
-
-    assert len(cds_parts[1]) == (50 - 30 + 1) - 2
 
 
 def test_full_transcript(parser):
     transcripts = parser.parse_transcripts()
-    fasta_file = "test/fixtures/testfasta.fasta"
     transcript = transcripts[0]
-    cds_parts = list(iter_cds_sequences(fasta_file, transcript))
 
-    full_sequence = "".join(cds_parts)
+    genome = {
+        record.id: record
+        for record in SeqIO.parse("test/fixtures/testfasta.fasta", "fasta")
+    }
+
+    full_sequence = get_full_sequence(genome, transcript)
     assert len(full_sequence) % 3 == 0
