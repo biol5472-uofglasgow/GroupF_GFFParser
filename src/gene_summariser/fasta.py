a2f43804920e1581745fcb81d59ec5d34b9820f1
ABEL BIJU DANIEL <132029020+DarkATOM7516@users.noreply.github.com.>
2026-01-28 18:14:13 +0000

    Fixed numpy idex error in fasta sequence extraction

--- src/gene_summariser/fasta.py
+++ src/gene_summariser/fasta.py
@@ -2,11 +2,13 @@ from collections.abc import Iterator
 
 from Bio import SeqIO
 from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+
 
 from gene_summariser.models import Transcript
 
 
-def get_full_sequence(genome: dict[str, Seq], transcript: Transcript) -> str:
+def get_full_sequence(genome: dict[str, SeqRecord], transcript: Transcript) -> str:
     """
     Extract the full spliced CDS (no phase),
     suitable for start/stop codon QC.
@@ -20,7 +22,12 @@ def get_full_sequence(genome: dict[str, Seq], transcript: Transcript) -> str:
 
     for cds in transcript.cds_features:
         record = genome[cds.seqid]
-        seq_chunks.append(record.seq[cds.start - 1 : cds.end])
+
+        seq=record.seq
+        if seq is None:
+            raise ValueError(f"No sequence found for {cds.seqid}")
+
+        seq_chunks.append(seq[cds.start - 1 : cds.end])
 
     full_sequence = sum(seq_chunks, Seq(""))
 
