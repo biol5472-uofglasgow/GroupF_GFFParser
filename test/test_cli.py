97187e589c12c479e700b2ae82b8a58371818856
Callum Poole <CallumPoole@protonmail.com>
2026-01-29 20:48:03 +0000

    added additional tests for the cli, these focus more on whether the cli runs or exits correctly, rather than the actul output

--- test/test_cli.py
+++ test/test_cli.py
@@ -4,6 +4,8 @@ from gene_summariser.cli import main
 
 """
 Unit tests for the CLI work slightly different due the argparse usage.
+As alot of the actual output from the CLI is tested in the integration test
+this mainly tests if the CLI runs or exits correctly without worrying about output.
 """
 
 
@@ -20,7 +22,7 @@ def test_cli_correct_gff(monkeypatch) -> None:
     try:
         main()
     except SystemExit as e:
-        assert e.code == 0  # Expecting normal exit
+        assert e.code == 0
 
 
 def test_cli_incorrect_gff(monkeypatch) -> None:
@@ -36,4 +38,42 @@ def test_cli_incorrect_gff(monkeypatch) -> None:
     try:
         main()
     except SystemExit as e:
-        assert e.code == 1  # Expecting error exit
+        assert e.code == 1
+
+
+def test_cli_help(monkeypatch) -> None:
+    """
+    Test that cli help works
+    """
+    monkeypatch.setattr(
+        sys,
+        "argv",
+        ["gene_summariser", "--help"],
+    )
+
+    try:
+        main()
+    except SystemExit as e:
+        assert e.code == 0
+
+
+def test_cli_strict_mode(monkeypatch) -> None:
+    """
+    Test that cli fails in strict mode when flags are present
+    2 transcripts in the test gff have qc flags so should exit with code 2
+    """
+    monkeypatch.setattr(
+        sys,
+        "argv",
+        [
+            "gene_summariser",
+            "--gff",
+            "test/fixtures/models.gff3",
+            "--strict",
+        ],
+    )
+
+    try:
+        main()
+    except SystemExit as e:
+        assert e.code == 2
